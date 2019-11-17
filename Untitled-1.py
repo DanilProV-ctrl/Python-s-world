2183ae6a6fd653b67a43e3fdac542c8d4786ca90
DanilProV-ctrl <ProVDanil2019@yandex.ru>
2019-11-17 13:41:21 +1000

    -

---
+++ Untitled-1.py
@@ -0,0 +1,42 @@
+age = 35
+name = "Danilian III"
+what = input( "Who are you?" )
+
+print("Okey," + what + ",My Name Is - Danilian III," + "and my age - " + str(age))
+
+a = 78
+b = 65.97
+c,d = 78.7,300
+print(a,b,c,d)
+
+a += 2
+b /= 2
+print(a,b)
+#constanta
+Width = 800
+Heigh = 600
+p = "Vasa"
+print(str(p))
+#other types(int,float)
+print(type(a))
+g = a + b
+print(g)  #arifmetika
+#pep8-pycodestyle(proverka komand)
+#peremeniye-mogno cherez zapatuy
+#Constanta - peremennaya,kotoroe v processe coda ne menyaetsa(oboznachenye,kotoroe nujno dlya teba y parnya,cnotrashego tvoy comp)
+#sverhu vniz
+#'''randomayzer
+#'''
+'''h
+t
+m
+l
++
+p
+y
+t
+h
+o
+n'''
+k = l = 96
+print(k,l)
